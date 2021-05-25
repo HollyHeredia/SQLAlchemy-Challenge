a6f0f67b8e2ce9865bd707e059be4ab9c9e56358
Holly Heredia <hollyheredia@gmail.com>
2021-05-25 16:26:59 -0700

    Added code for /tobs portion of flask part of the challenge.

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import numpy as np
+import datetime as dt
 
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
@@ -16,20 +17,21 @@ Base = automap_base()
 Base.prepare(engine, reflect=True)
 
 # Save reference to the table
-stations = Base.classes.station
+station = Base.classes.station
 measurement = Base.classes.measurement
 
 # Flask Setup
 app = Flask(__name__)
 
 # Flask Routes
+@app.route("/")
 def welcome():
     """List all available api routes."""
     return (
         f"Available Routes:<br/>"
         f"/api/v1.0/stations<br/>"
         f"/api/v1.0/precipitation<br/>"
-        f"/api/v1.0/tobs<br/>
+        f"/api/v1.0/tobs<br/>"
         f"/api/v1.0/<start>/<end>"
     )
 @app.route("/api/v1.0/stations")
@@ -37,13 +39,13 @@ def stations():
     # Create our session (link) from Python to the DB
     session = Session(engine)
 
-    """Return a list of passenger data including the name, age, and sex of each passenger"""
-    # Query all passengers
-    results = session.query(stations.station, stations.name).all()
+    """Return a list of station data including station, the name"""
+    # Query all station
+    results = session.query(station.station, station.name).all()
 
     session.close()
 
-    # Create a dictionary from the row data and append to a list of all_passengers
+    # Create a dictionary from the row data and append to a list of all_stations
     all_stations = []
     for station, name in results:
         station_dict = {}
@@ -76,13 +78,35 @@ def preciptations():
 
 @app.route("/api/v1.0/tobs")
 # Query the dates and temperature observations of the most active station for the last year of data.
+def tobs():
+    session = Session(engine)
+
+    active_stations_descending = session.query(measurement.station, func.count(measurement.station)).\
+                group_by(measurement.station).order_by(func.count(measurement.station).desc()).all()
+
+    most_active_station = active_stations_descending[0][0]
+
+    stationsummary = session.query(measurement.station, measurement.date, measurement.tobs).\
+        filter(measurement.station == most_active_station).all()
+
+    last_date = session.query(measurement.date).order_by(measurement.date.desc()).first().date
+    last_year = dt.datetime.strptime(last_date, '%Y-%m-%d') - dt.timedelta(days=365)
+
+    temp_results = session.query(measurement.date, measurement.tobs).\
+        filter(measurement.station == most_active_station).filter(measurement.date >= last_year).\
+        group_by(measurement.date).all()
+
+        
+
+    print(f"For temperatures for the last year of station {most_active_station}:")
+    return jsonify(temp_results)
 
 
 
 # Return a JSON list of temperature observations (TOBS) for the previous year.
 
-@app.route("/api/v1.0/<start>")
-@app.route("/api/v1.0/<start>/<end>")
+#@app.route("/api/v1.0/<start>")
+#@app.route("/api/v1.0/<start>/<end>")
 
 #Return a JSON list of the minimum temperature, the average temperature, and the max temperature for a given start or start-end range.
 
